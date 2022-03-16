fb0c31235ceb01f18a8c6ab9e61473dd133dadb4
AlessioPampana <apampana@es-tek.it>
2022-03-16 18:07:07 +0100

    first

---
+++ src/plugins/vuetify.js
@@ -0,0 +1,24 @@
+import Vue from 'vue';
+import Vuetify from 'vuetify/lib/framework';
+
+Vue.use(Vuetify);
+
+export default new Vuetify({
+   /* theme: {
+        themes: {
+            light: {
+                color1: '#FFFFFF',//white
+                color2: '#8ECA3B',//green
+                color3: '#64646C',//gray
+                accent: '#8c9eff',
+                error: '#b71c1c',
+            },
+            dark: {
+                primary: '#000000',
+                secondary: '#000000',
+                accent: '#000000',
+                error: '#000000',
+            },
+        },
+    },*/
+});

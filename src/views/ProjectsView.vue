0f1f53cf865554f0c9b85407a4153aba8b78b3af
AlessioPampana <apampana@es-tek.it>
2022-03-18 16:16:29 +0100

    a

--- src/views/ProjectsView.vue
+++ src/views/ProjectsView.vue
@@ -123,14 +123,14 @@
     </template>
     <template v-slot:[`item.actions`]="{ item }">
       <v-icon
-        small
+        
         class="mr-2"
         @click="editItem(item)"
       >
         mdi-pencil
       </v-icon>
       <v-icon
-        small
+        
         @click="deleteItem(item)"
       >
         mdi-delete
